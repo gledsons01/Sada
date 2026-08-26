229cdfa29001a2b281c1308d17fe111cd6281c20
Gledson Luiz <gledson.souza_ext@solagora.com.br>
2026-08-26 03:28:12 -0300

    Inclusão das Configurações do Angular.

---
+++ Sada.Web/Sada.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Sada.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
